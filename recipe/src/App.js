13675b54bc4c3e1e8865b77a6989a9f024f8d655
MehfoozAbbas110 <33392056+MehfoozAbbas110@users.noreply.github.com>
2024-06-25 12:14:37 +0530

    update

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddRecipe from './Components/AddRecipe';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path= "/addrecipe" element={<AddRecipe />} />
+        <Route path="/" element={ <AddRecipe /> }/>
       </Routes>
     </div>
   );
--- recipe/src/Components/AddRecipe.jsx
+++ recipe/src/Components/AddRecipe.jsx
@@ -1,5 +1,50 @@
-function addRecipe(params) {
-    return <div>
-        Add Recipe Page
+import "./style.css";
+
+
+function AddRecipe() {
+  return (
+    <div className="App">
+      <h1>Add Recipe Page</h1>
+      <form className="form__container">
+        <div className="form__controls">
+          <label htmlFor="title">Title : </label>
+          <input
+            type="text"
+            id="username"
+          />
+        </div>
+        <div className="form__controls">
+          <label htmlFor="description">Description : </label>
+          <input
+            type="text"
+            id="username"
+          />
+        </div>
+        <div className="form__controls">
+          <label htmlFor="ingridient">Ingredient : </label>
+          <input
+            type="text"
+            id="username"
+          />
+        </div>
+        <div className="form__controls">
+          <label htmlFor="preparation">Preparation Steps : </label>
+          <input
+            type="text"
+            id="username"
+          />
+        </div>
+        <div className="form__controls">
+        <label htmlFor="image">Upload Image : </label>
+            <input 
+              type="file" />
+        </div>
+        <div className="form__controls">
+          <button className="button">Add</button>
+        </div>
+      </form>
     </div>
+  );
 }
+
+export default AddRecipe;
